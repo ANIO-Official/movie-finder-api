ce251bd049b55a4304ec17cdf287054d981468c8
Amanda Ogletree <amandaogletree19@gmail.com>
2026-01-13 16:08:31 -0500

    Added API client in services directory for omdb api. Added movieController in controllers directory with functions for searching by movie title and id. Tested movie title query.

---
+++ controllers/movieController.js
@@ -0,0 +1,48 @@
+const omdbClient = require('../services/api-client')
+const key = process.env.OMDB_API_KEY
+
+async function searchMovies(req, res){
+    try{
+        const title = req.query.title //search term
+
+        if(!title ){ //Validate if title has been included in endpoint.
+            console.error('Error: API request failed due to missing title in parameters: ', error.message)
+            res.send(404).json({error: 'Title query parameter is required.'})
+        }//continue if title is included. V
+
+        const response = await omdbClient.get(`/?apikey=${key}&s=${title}`) //search by title
+        res.json(response.data)
+
+    }catch(error){
+        if(error.response){
+            console.error('API Error: ', error.response.status, error.response.data)
+            res.status(error.response.status).json({message: 'An API error occured while searching for movies.'})
+        }else{
+            console.error*('Network Error: ', error.message)
+            res.status(502).json({message: 'A Network error has occured.'})
+        }
+    }
+}
+
+async function getMovieDetails(req, res){
+    try{
+        const id = req.query.id //ID provided
+        const response = await omdbClient.get(`/?apikey=${key}&i=${id}`)
+
+        res.json(response.data)
+
+    }catch(error){
+        if(error.response){
+            console.error('API Error: ', error.response.status, error.response.data)
+            res.status(error.response.status).json({message: 'An API error occured while acquiring movie details. We apologize.'})
+        }else{
+            console.error*('Network Error: ', error.message)
+            res.status(502).json({message: 'A Network error has occured.'})
+        }
+    }
+}
+
+module.exports = {
+    searchMovies,
+    getMovieDetails
+}
---
+++ routes/movieRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const movieController = require('../controllers/movieController')
+
+router.get('/search', movieController.searchMovies)
+
+router.get('/movies/:id', movieController.getMovieDetails)
+
+module.exports = router
